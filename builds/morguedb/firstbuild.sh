98ca2a3c57cda54df0c07459beb0c665a135db29
Antonino Abbate <aabbate@vendini.com>
2017-01-30 18:16:16 +0100

    Updated morguedb build script

--- builds/morguedb/firstbuild.sh
+++ builds/morguedb/firstbuild.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Set the tag with current date
+TAG=$(date +"%Y.%m.%d-%H.%M.%S")
+
 # Start mariadb basic container
 docker run -d -p 3306:3306 --name mariadb aabbate/mariadb 
 
@@ -13,6 +16,7 @@ docker exec mariadb "/init.sh"
 
 # Push the base container to aabbate/morguedb
 if docker ps -a | grep -q mariadb ; then
-	docker commit -p mariadb aabbate/morguedb
-    docker push aabbate/morguedb
+	docker commit -p mariadb aabbate/morguedb:${TAG}
+	docker push aabbate/morguedb:${TAG}
+	docker push aabbate/morguedb:latest
 fi
